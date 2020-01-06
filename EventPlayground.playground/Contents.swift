8160f3428e106b5f329d2a31ed0d10444ecc201e
Shawn Roller <sroller@TechStyle.com>
2020-01-06 16:41:08 -0500

    initial commit

---
+++ EventPlayground.playground/Contents.swift
@@ -0,0 +1,76 @@
+import SwiftUI
+import PlaygroundSupport
+
+// switch
+struct ContentView1: View {
+    @State private var showGreeting = true
+    
+    var body: some View {
+        VStack {
+            Toggle(isOn: $showGreeting) {
+                Text("Show welcome message")
+            }
+            .padding()
+            
+            if showGreeting {
+                Text("Hallo warld")
+            }
+        }
+    }
+}
+
+// button
+struct ContentView2: View {
+    @State private var showDetails = false
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                self.showDetails.toggle()
+            }) {
+                HStack {
+                    Text("Show details")
+                    Image(systemName: "cloud.fill")
+                }
+            }
+            
+            Button(action: {
+                self.showDetails.toggle()
+            }) {
+                HStack {
+                    Text("Show details2")
+                    Image(systemName: "cloud.fill")
+                }
+            }
+            .buttonStyle(PlainButtonStyle()) // hide the automatic button style tint
+            
+            if showDetails {
+                Text("do sumting")
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
+// nav button
+struct ContentView3: View {
+    @State private var showDetails = false
+    
+    var body: some View {
+        NavigationView {
+            HStack {
+                NavigationLink(destination: Text("detail view here")) {
+                    Image(systemName: "cloud")
+                }
+                .buttonStyle(PlainButtonStyle()) // hide the automatic button style tint
+                
+                NavigationLink(destination: Text("poopoo")) {
+                    Image(systemName: "cloud.fill")
+                        .renderingMode(.original) // hide the automatic button style tint - this keeps the entire button tappable when inside a list
+                }
+            }
+        }
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView3())
